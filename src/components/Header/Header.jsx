873dc6306392843e5d397da9991bbc1a7c6feec2
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-20 12:00:54 +0000

    render random adjectives

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,19 @@
 import "./Header.css";
 
+const descriptiveAdjs = ["Core", "Fundamental", "Crucial"];
+
+function getRandomAdjective(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 export default function Header() {
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
+        {descriptiveAdjs[getRandomAdjective(3)]} React concepts you will need
+        for almost any app you are going to build!
       </p>
     </header>
   );
