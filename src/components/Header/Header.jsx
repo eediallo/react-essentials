758b038e14824f687dbf753040985c2a8eebc042
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-20 12:03:08 +0000

    refactor by import core concpet image

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import "./Header.css";
 
 const descriptiveAdjs = ["Core", "Fundamental", "Crucial"];
+import CoreConceptImg from "../../assets/react-core-concepts.png";
 
 function getRandomAdjective(max) {
   return Math.floor(Math.random() * (max + 1));
@@ -9,7 +10,7 @@ function getRandomAdjective(max) {
 export default function Header() {
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={CoreConceptImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {descriptiveAdjs[getRandomAdjective(3)]} React concepts you will need
